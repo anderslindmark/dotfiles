4ead257bd6d8f318ca73369d741c1c7a123fad21
Anders Lindmark <anders.lindmark@gmail.com>
2015-07-15 13:54:40 +0200

    Dont do weird stuff with .git

--- init.sh
+++ init.sh
@@ -1,12 +1,15 @@
 #!/bin/zsh
+setopt EXTENDED_GLOB
+
 mkdir ~/dotfilebackup 2> /dev/null 
 if [ $? -ne 0 ]; then
 	echo "Could not create backup-directory, ~/dotfilebackup, old one still available."
 	exit 1
 fi
 
-for file (.*); do
+for file in .^git; do
 	if [[ -e ~/$file ]]; then
+		echo "Backing up $file"
 		mv ~/$file ~/dotfilebackup/
 	fi
 	ln -s ~/dotfiles/$file ~/$file
