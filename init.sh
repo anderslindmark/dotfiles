4a0b7ee4eaf875b72f8f2066b0f5d3089151cb05
Anders Lindmark <anders.lindmark@gmail.com>
2015-07-15 17:07:49 +0200

    Check for already linked dotfiles

--- init.sh
+++ init.sh
@@ -1,27 +1,39 @@
 #!/bin/zsh
 setopt EXTENDED_GLOB
 
+DOTFILE_DIR=~/dotfiles
+BACKUP_DIR=~/dotfiles/backup
+
 if [[ $(pwd) == $(realpath ~) ]]; then
-	echo "Dont use this command from ~/. Do \"cd dotfiles\" first"
+	echo "Dont use this command from ~/. Do \"cd $DOTFILE_DIR\" first"
 	exit 1
 fi
 
-mkdir ~/dotfilebackup 2> /dev/null 
-if [ $? -ne 0 ]; then
-	echo "Could not create backup-directory, ~/dotfilebackup, old one still available."
-	exit 1
-fi
+mkdir BACKUP_DIR 2> /dev/null 
 
 for file in .^git; do
 	if [[ -e ~/$file ]]; then
-		echo "Backing up $file"
-		mv ~/$file ~/dotfilebackup/
+		if [[ -h ~/$file ]]; then
+			# $file is a symlink, check if target is the dotfiles-one
+			if [[ $(readlink ~/$file) == $DOTFILE_DIR/$file ]]; then
+				echo "$file already added, skipping"
+				continue
+			else
+				echo "unlinking $file"
+				unlink ~/$file
+			fi
+		else
+			# regular file or directory, back it up
+			echo "Backing up $file"
+			mv ~/$file $BACKUP_DIR/
+		fi
 	fi
-	ln -s ~/dotfiles/$file ~/$file
+	echo "linking $DOTFILE_DIR/$file to ~/$file"
+	ln -s $DOTFILE_DIR/$file ~/$file
 done
 
-rmdir ~/dotfilebackup 2> /dev/null 
+rmdir $BACKUP_DIR 2> /dev/null 
 if [ $? -ne 0 ]; then; 
-	echo "Old dotfiles are available in ~/dotfilebackup" 
+	echo "Old dotfiles are available in $BACKUP_DIR" 
 fi
 
