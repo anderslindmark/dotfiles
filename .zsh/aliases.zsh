ba61990ebe59d764248eedb59ba1b1ac08305de5
Anders Lindmark <anders.lindmark@gmail.com>
2015-07-21 02:46:25 +0200

    Separate host-specific stuff

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,3 +1,4 @@
+# Platform specific
 case `uname` in
 	Darwin)
 		alias ll="ls -lAhG"
@@ -6,9 +7,15 @@ case `uname` in
 		alias ll="ls -lAh --color"
 esac
 
+# Host specific
+if [[ $(hostname) == localghost ]]; then
+	alias pure-ftpwho="sudo docker exec -ti ftp_server /usr/sbin/pure-ftpwho"
+fi
+
+# General
 alias l=ll
 alias c="gcal -K -s 1 ."
 alias d="date +'%F %H:%M'"
-alias pure-ftpwho="sudo docker exec -ti ftp_server /usr/sbin/pure-ftpwho"
 alias line="~/bin/line"
 alias wol_macbook="wakeonlan 01:25:BC:E7:05:2A"
+
